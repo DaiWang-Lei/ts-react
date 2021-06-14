683a5b1575869b82296debf9b2fcf1c6344f030b
罗卜大 <7628035+wx_8387aaaa57@user.noreply.gitee.com>
2021-06-15 07:25:43 +0800

    ref前准备

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -6,15 +6,21 @@ const LikeButton: React.FC = () => {
   const positions = useMousePosition();
   useEffect(() => {
     console.log('uesEffect is running');
-
     document.title = `点击了${like}次`
   }, [like, on])
+
+  function handleAlertClick() {
+    setTimeout(() => {
+      alert(`you clicked on ${like}次`)
+    }, 3000);
+  }
   return (
     <>
       <h2>X:{positions.x},Y:{positions.y}</h2>
       <button onClick={() => setLike(like + 1)}>
         {like}👍
       </button>
+      <button onClick={handleAlertClick}>Alert!</button>
       <button onClick={() => setOn(!on)}>
         {on ? 'on' : 'off'}
       </button>
